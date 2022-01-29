8534a4665eead43780af3227e900203655019af9
zzttzz <2775859312@qq.com>
2022-01-29 11:03:52 +0800

    完善预设值编译环境脚本

--- setenv.sh
+++ setenv.sh
@@ -4,11 +4,17 @@
  # @Autor: ZZT
  # @Date: 2022-01-29 10:54:32
  # @LastEditors: ZZT
- # @LastEditTime: 2022-01-29 10:58:41
+ # @LastEditTime: 2022-01-29 11:03:01
 ### 
 
 env_file=/home/zzt/gcc-for-arm/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.sh
 
 source ${env_file}
 
-bash
+if [ $SHELL == "/bin/zsh" ]; then
+    zsh;
+elif [ $SHELL == "/bin/csh" ]; then
+    csh;
+else
+    bash;
+fi
